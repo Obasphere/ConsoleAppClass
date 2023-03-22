ad3d9a1650a21533e74a3484cc79539bb8af22d4
Damilola Oba Adesanya <dami.d.adesanya@gmail.com>
2023-03-22 19:04:01 +0100

    Add project files.

---
+++ ConsoleAppClass/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Service.SaveController;
+
+namespace ConsoleAppClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string samples
+            string Joyce = "Joyce First App";
+            string joyce = "Joyce(jnr.) First App";
+            string joyce2 = "667";
+
+
+            Console.WriteLine(joyce);
+            Console.WriteLine(Joyce);
+            Console.WriteLine(joyce2);
+
+            // integer samples
+            int A = 7;
+            int B = 444;
+
+            Console.WriteLine(A);
+            A = 9;
+
+            Console.WriteLine(A);
+            Console.WriteLine(A + B);
+
+            if(A > B)
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {A}");
+            }
+            else
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {B}");
+            }
+
+            if(A > B)
+            {
+                Console.WriteLine("A is Awesome, cos it equals {A}");
+            }
+            else
+            {
+                Console.WriteLine("A is Awesome, cos it equals {B}");
+            }
+
+            //A > B ? "B" : "A";
+
+            if (A > B)
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {A}");
+            }
+            else if (A < B)
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {B}");
+            }
+
+            if (A > B)
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {A}");
+            }
+            if (A < B)
+            {
+                Console.WriteLine($"A is Awesome, cos it equals {B}");
+            }
+
+            //concatenate
+            Console.WriteLine($"{A} + {B}");
+            Console.WriteLine(A + " is a good number for" + " " + Joyce);
+            Console.WriteLine($"{A} is a good number for {Joyce}");
+
+            //Console.WriteLine("My first App");
+
+
+            //Console.WriteLine("My second App");
+
+            //Console.WriteLine("My third App");
+            //Console.WriteLine(2 + 45);
+            //Console.WriteLine(3 < 2);
+            //Console.WriteLine(3  > 2);
+            //Console.WriteLine(3 < 2);
+            //Console.WriteLine(3 == 2);
+            //Console.WriteLine(3 <= 2);
+        }
+    }
+}
